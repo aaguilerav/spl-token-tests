0eeef4c0ee0047fc319c0fa336fe0ef60bdf9a64
Alex Aguilera Vega <aav@powerledger.io>
2023-01-16 16:56:43 +0800

    Well, seems to be fixed

--- start.js
+++ start.js
@@ -28,8 +28,8 @@ const payer_privkey = require('./helpers/pk-payer.json');
 const token_holder_privkey = require('./helpers/pk-token-holder.json');
 const mintauth_privkey = require('./helpers/pk-mint-authority.json');
 const freezeauth_privkey = require('./helpers/pk-freeze-authority.json');
-const TOKEN_PROGRAM = new PublicKey('TokeRNc22SySPXzDxhu4geGyqsXqv8MHutdMq3MAn7Q');
-const ATOKEN_PROGRAM = new PublicKey('ATokLqXRe9wAGn9cTACPHewKB5LkhTu8WvcbPivLVNBA');
+const TOKEN_PROGRAM = new PublicKey('9qgWVnARZLqQkj4iHJqWw3fSzEmnU37FqKzuckpU2pvk');
+const ATOKEN_PROGRAM = new PublicKey('BAKPYb2dKnMPZeHLizEsLeoE8jPdej7tvGkZE38i9us7');
 
 // Implementation taken from: node_modules/@solana/spl-token/src/instructions/associatedTokenAccount.ts:17
 const buildAssociatedTokenAccountInstructionForked = (
@@ -118,7 +118,7 @@ const main = async () => {
         'confirmed',
         TOKEN_PROGRAM
     );
-    log();
+    log(`1) ----------------------------------------------------------------`);
     log(`spl_token_address address: ${mint}`);
     log(`mintInfo.address ${mintInfo.address.toBase58()}`);
     log(`mintInfo.mintAuthority ${mintInfo.mintAuthority.toBase58()}`);
@@ -130,6 +130,7 @@ const main = async () => {
 
     // STEP-3 Get/Create ATOKEN Account. (Manually doing what getOrCreateAssociatedTokenAccount would do)
     // Implementation taken from: node_modules/@solana/spl-token/src/actions/getOrCreateAssociatedTokenAccount.ts:30 (getOrCreateAssociatedTokenAccount(...))
+    log(`2) ----------------------------------------------------------------`);
     const associatedToken = await getAssociatedTokenAddress(
         mint,
         token_holder.publicKey,
@@ -137,98 +138,135 @@ const main = async () => {
         TOKEN_PROGRAM,
         ATOKEN_PROGRAM
     );
-
     log(`getAssociatedTokenAddress: ${associatedToken}`);
 
-    try {
-        const transaction = new Transaction().add(
-            createAssociatedTokenAccountInstructionForked(
-                payer.publicKey,
-                associatedToken,
-                token_holder.publicKey,
-                mint,
-                TOKEN_PROGRAM,
-                ATOKEN_PROGRAM
-            )
-        );
-        await sendAndConfirmTransaction(
-            connection,
-            transaction,
-            [payer],
-            {
-                commitment: 'confirmed',
-                maxRetries: 3
-            });
-    } catch (error) {
-        log(`error: ${error}`);
-    }
-    log(`getAccountInfo: ${await connection.getAccountInfo(associatedToken, 'confirmed')}`);
-
     // STEP-3 Get/Create ATOKEN Account.
-    // log('getOrCreateAssociatedTokenAccount');
-    // let tokenAccount = await getOrCreateAssociatedTokenAccount(
-    //     connection,
-    //     payer,
-    //     mint,
-    //     payer.publicKey,
-    //     TOKEN_PROGRAM,
-    //     ATOKEN_PROGRAM
-    // );
-    // log();
-    // log(`tokenAccount.address ${tokenAccount.address.toBase58()}`);
-    // log(`tokenAccount.mint ${tokenAccount.mint.toBase58()}`);
-    // log(`tokenAccount.owner ${tokenAccount.owner.toBase58()}`);
-    // log(`tokenAccount.amount ${tokenAccount.amount}`);
-    // log();
+    log(`3) ----------------------------------------------------------------`);
+    // try {
+    //     const transaction = new Transaction().add(
+    //         createAssociatedTokenAccountInstructionForked(
+    //             payer.publicKey,
+    //             associatedToken,
+    //             token_holder.publicKey,
+    //             mint,
+    //             TOKEN_PROGRAM,
+    //             ATOKEN_PROGRAM
+    //         )
+    //     );
+    //     const res = await sendAndConfirmTransaction(
+    //         connection,
+    //         transaction,
+    //         [payer],
+            // {
+            //     commitment: 'confirmed',
+            //     maxRetries: 3
+            // });
+    //     log(res);
+    // } catch (error) {
+    //     log(`error: ${error}`);
+    // }
 
-    // STEP-4 Get Atoken Account info
-    // log('tokenAccountInfo');
+    log(`4) ----------------------------------------------------------------`);
+
+    // let accInfo = await connection.getAccountInfo(associatedToken, 'confirmed');
+    // log(accInfo);
+    // log(`${accInfo.owner}, ${TOKEN_PROGRAM}, ${accInfo.owner.equals(TOKEN_PROGRAM)}`);
+
     // let tokenAccountInfo = await getAccount(
     //     connection,
-    //     tokenAccount.address
-    // );
-    // log();
-    // log(`tokenAccountInfo.address ${tokenAccountInfo.address.toBase58()}`);
-    // log(`tokenAccountInfo.mint ${tokenAccountInfo.mint.toBase58()}`);
-    // log(`tokenAccountInfo.owner ${tokenAccountInfo.owner.toBase58()}`);
-    // log(`tokenAccountInfo.amount ${tokenAccountInfo.amount}`);
-    // log();
+    //     associatedToken,
+    //     'confirmed',
+    //     TOKEN_PROGRAM);
+
+    // log(tokenAccountInfo);
+
+    log(`5) ----------------------------------------------------------------`);
+
+    // STEP-3 Get/Create ATOKEN Account.
+    log('getOrCreateAssociatedTokenAccount');
+
+    let tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mint,
+        payer.publicKey,
+        false,
+        'confirmed',
+        {maxRetries: 3},
+        TOKEN_PROGRAM,
+        ATOKEN_PROGRAM
+    );
+    log();
+    log(`tokenAccount.address ${tokenAccount.address.toBase58()}`);
+    log(`tokenAccount.mint ${tokenAccount.mint.toBase58()}`);
+    log(`tokenAccount.owner ${tokenAccount.owner.toBase58()}`);
+    log(`tokenAccount.amount ${tokenAccount.amount}`);
+    log();
+
+    log(`6) ----------------------------------------------------------------`);
+    // STEP-4 Get Atoken Account info
+    log('tokenAccountInfo');
+
+    let tokenAccountInfo = await getAccount(
+        connection,
+        tokenAccount.address,
+        'confirmed',
+        TOKEN_PROGRAM
+    );
+    log();
+    log(`tokenAccountInfo.address ${tokenAccountInfo.address.toBase58()}`);
+    log(`tokenAccountInfo.mint ${tokenAccountInfo.mint.toBase58()}`);
+    log(`tokenAccountInfo.owner ${tokenAccountInfo.owner.toBase58()}`);
+    log(`tokenAccountInfo.amount ${tokenAccountInfo.amount}`);
+    log();
+
+    log(`7) ----------------------------------------------------------------`);
 
     // STEP-5 Mint tokens and grant them to tokenAccount.address
-    // await mintTo(
-    //     connection,
-    //     payer,
-    //     mint,
-    //     tokenAccount.address,
-    //     mintAuthority,
-    //     100000000000 // because decimals for the mint are set to 9 
-    // );
+    await mintTo(
+        connection,
+        payer,
+        mint,
+        tokenAccount.address,
+        mintAuthority,
+        100000000000,
+        [],
+        {commitment: 'confirmed', maxRetries: 3},
+        TOKEN_PROGRAM
+    );
 
+    log(`8) ----------------------------------------------------------------`);
     // STEP-6 Get Mint info and validate new supply of tokens
-    // mintInfo = await getMint(
-    //     connection,
-    //     mint
-    // );
-    // log();
-    // log(`mintInfo.address ${mintInfo.address.toBase58()}`);
-    // log(`mintInfo.mintAuthority ${mintInfo.mintAuthority.toBase58()}`);
-    // log(`mintInfo.freezeAuthority ${mintInfo.freezeAuthority.toBase58()}`);
-    // log(`mintInfo.supply ${mintInfo.supply}`);
-    // log(`mintInfo.decimals ${mintInfo.decimals}`);
-    // log(`mintInfo.isInitialized ${mintInfo.isInitialized}`);
-    // log();
 
-    // STEP-7 Get tokenAccount.address account info and validate new supply of tokens
-    // tokenAccountInfo = await getAccount(
-    //     connection,
-    //     tokenAccount.address
-    // );
-    // log();
-    // log(`tokenAccountInfo.address ${tokenAccountInfo.address.toBase58()}`);
-    // log(`tokenAccountInfo.mint ${tokenAccountInfo.mint.toBase58()}`);
-    // log(`tokenAccountInfo.owner ${tokenAccountInfo.owner.toBase58()}`);
-    // log(`tokenAccountInfo.amount ${tokenAccountInfo.amount}`);
-    // log();
+    mintInfo = await getMint(
+        connection,
+        mint,
+        'confirmed',
+        TOKEN_PROGRAM
+    );
+    log();
+    log(`mintInfo.address ${mintInfo.address.toBase58()}`);
+    log(`mintInfo.mintAuthority ${mintInfo.mintAuthority.toBase58()}`);
+    log(`mintInfo.freezeAuthority ${mintInfo.freezeAuthority.toBase58()}`);
+    log(`mintInfo.supply ${mintInfo.supply}`);
+    log(`mintInfo.decimals ${mintInfo.decimals}`);
+    log(`mintInfo.isInitialized ${mintInfo.isInitialized}`);
+    log();
+
+    log(`9) ----------------------------------------------------------------`);
+    // // STEP-7 Get tokenAccount.address account info and validate new supply of tokens
+    tokenAccountInfo = await getAccount(
+        connection,
+        tokenAccount.address,
+        'confirmed',
+        TOKEN_PROGRAM
+    );
+    log();
+    log(`tokenAccountInfo.address ${tokenAccountInfo.address.toBase58()}`);
+    log(`tokenAccountInfo.mint ${tokenAccountInfo.mint.toBase58()}`);
+    log(`tokenAccountInfo.owner ${tokenAccountInfo.owner.toBase58()}`);
+    log(`tokenAccountInfo.amount ${tokenAccountInfo.amount}`);
+    log();
 }
 
 /**
